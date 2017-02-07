e2b74021e42d96fd50e58d38bf453ffac655ade3
Abhinav <singh09.abhinav@gmailcom>
2017-02-08 00:35:00 +0530

    First Commit/ 7_Feb_2017

---
+++ src/main/java/org/abhinav/javabrains/messenger/resources/InjectDemoResource.java
@@ -0,0 +1,37 @@
+package org.abhinav.javabrains.messenger.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/injectdemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class InjectDemoResource {
+	
+	@GET
+	@Path("annotations")
+	public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
+											@HeaderParam("authSessionId") String header,
+											@CookieParam("name") String cookie){
+		return "Matrix Param: "+matrixParam+ " Header Param: "+header+ " cookie: "+cookie;
+	}
+	
+	@GET
+	@Path("context")
+	public String getParamsUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders header ){
+		String path= uriInfo.getAbsolutePath().toString();
+		String cookies =header.getCookies().toString();
+		return "Path: "+ path+ " Cookie: "+cookies;
+	}
+	
+
+}
